222f2f6afb247cf83067416ca43833fcc2ee4a5f
Philipp A. <flying-sheep@web.de>
2025-10-18 15:25:31 +0200

    load JS globally

--- src/sphinx_exec_jupyter/__init__.py
+++ src/sphinx_exec_jupyter/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import json
 from importlib.metadata import version
+from importlib.resources import files
 from itertools import batched
 from typing import TYPE_CHECKING, TypedDict
 
@@ -11,7 +12,10 @@ import myst_nb.docutils_
 import myst_nb.sphinx_ext
 from docutils import nodes
 from docutils.parsers.rst import directives
+from myst_nb.sphinx_ import NbMetadataCollector
 from nbformat import v4
+from panel.io.convert import BOKEH_VERSION
+from panel.io.resources import CDN_DIST
 from sphinx.util.docutils import SphinxDirective
 from sphinx.util.typing import ExtensionMetadata
 from sphinx_design.shared import create_component
@@ -22,6 +26,21 @@ if TYPE_CHECKING:
     from sphinx.application import Sphinx
 
 __all__ = ["setup", "HoloviewsDirective"]
+
+FILES = files(__name__)
+COLLECT_URLS = (FILES / "collect-urls.py").read_text()
+
+# from https://github.com/holoviz-dev/nbsite/blob/e75708a28d9a4ab805753c0520b8eb8779c79d82/nbsite/pyodide/__init__.py#L82
+JS_URLS = {
+    "bokeh": [
+        f"https://cdn.bokeh.org/bokeh/release/bokeh-{BOKEH_VERSION}.min.js",
+        f"https://cdn.bokeh.org/bokeh/release/bokeh-widgets-{BOKEH_VERSION}.min.js",
+        f"https://cdn.bokeh.org/bokeh/release/bokeh-tables-{BOKEH_VERSION}.min.js",
+        f"{CDN_DIST}panel.min.js",
+    ],
+    "plotly": [],
+    "matplotlib": [],
+}
 
 
 def execute_cells(cells: list[str], document: nodes.document) -> list[nodes.Node]:
@@ -68,25 +87,34 @@ class HoloviewsDirective(SphinxDirective):
                 for block in [
                     f"import holoviews as hv\nhv.extension({backend!r})",
                     code,
+                    COLLECT_URLS,
                 ]
             ],
             self.state.document,
         )
 
-        results: list[tuple[nodes.Node, nodes.Node]] = []
-        for header, plot in batched(results_raw, 2, strict=True):
-            _code, header = header.children  # discard header code
-            results.append((header, plot))
+        urls = {"js": [url for b in backends for url in JS_URLS[b]], "css": []}
+        results: list[nodes.Node] = []
+        for _header, plot, urls_cell in batched(results_raw, 3, strict=True):
+            # container → output (second child) → literal (first child) → text
+            new_urls = json.loads(urls_cell.children[1].children[0].astext())
+            urls["js"] += new_urls["js"]
+            urls["css"] += new_urls["css"]
+            results.append(plot)
+
+        for url in urls["js"]:
+            key = f"holoviews-{url}"
+            NbMetadataCollector.add_js_file(self.env, self.env.docname, key, url, {})
 
         if len(results) == 1:
             return list(results[0])
 
         tab_set = create_component("tab-set", classes=["sd-tab-set"])
-        for i, (tab_name, (header, plot)) in enumerate(zip(backends, results)):
+        for i, (tab_name, plot) in enumerate(zip(backends, results)):
             textnodes, _ = self.state.inline_text(tab_name, self.lineno)
             tab_label = nodes.rubric(tab_name, "", *textnodes, classes=["sd-tab-label"])
             tab_content = create_component(
-                "tab-content", classes=["sd-tab-content"], children=[header, plot]
+                "tab-content", classes=["sd-tab-content"], children=[plot]
             )
             tab_item = create_component(
                 "tab-item",
