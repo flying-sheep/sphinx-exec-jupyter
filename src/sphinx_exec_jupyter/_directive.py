4ec1cff169066d1dbe068be1db7a233fb1fbb5ac
Philipp A. <flying-sheep@web.de>
2025-10-19 14:41:22 +0200

    Add basic directive (#3)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/sphinx_exec_jupyter/_directive.py
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: MPL-2.0
+from docutils import nodes
+from sphinx.util.docutils import SphinxDirective
+
+from .common import execute_cells
+
+__all__ = ["ExecJupyterDirective"]
+
+
+class ExecJupyterDirective(SphinxDirective):
+    has_content = True
+
+    def run(self) -> list[nodes.Node]:
+        code = "\n".join(self.content)
+        return execute_cells([code], self.state.document)
