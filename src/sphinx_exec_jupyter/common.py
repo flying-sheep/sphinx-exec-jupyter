79bb37a5584f057ff103bf1397df62760fc08d1f
Philipp A. <flying-sheep@web.de>
2025-10-23 12:16:48 +0200

    Better caching

--- src/sphinx_exec_jupyter/common.py
+++ src/sphinx_exec_jupyter/common.py
@@ -1,10 +1,11 @@
 # SPDX-License-Identifier: MPL-2.0
 from __future__ import annotations
 
+import shutil
 from contextlib import contextmanager
 from pathlib import Path
-from tempfile import mkstemp
-from typing import TYPE_CHECKING
+from tempfile import mkdtemp
+from typing import TYPE_CHECKING, TypedDict, cast
 
 import myst_nb.sphinx_
 from nbformat import v4
@@ -16,8 +17,15 @@ if TYPE_CHECKING:
     from myst_nb.sphinx_ import SphinxEnvType
 
 
+class ExtData(TypedDict, total=False):
+    count: int
+
+
 def execute_cells(
-    cells: list[str], document: nodes.document, *, env: SphinxEnvType
+    cells: list[str],
+    document: nodes.document,
+    *,
+    env: SphinxEnvType,
 ) -> list[nodes.Node]:
     """Execute code cells and return resulting docutils nodes, one per cell."""
     notebook_json = v4.writes(
@@ -28,11 +36,7 @@ def execute_cells(
     parser = myst_nb.sphinx_.Parser()
     parser.env = env
     after_last_child = len(document.children)
-    with (
-        temp_setting(parser.env.mystnb_config, "execution_mode", "force"),
-        temp_setting(parser.env.mystnb_config, "execution_raise_on_error", True),
-        temp_source_code(parser.env, notebook_json),
-    ):
+    with temp_source_code(parser.env, notebook_json):
         parser.parse(notebook_json, document)
 
     # extract nodes and restore document
@@ -45,47 +49,33 @@ def execute_cells(
     return nodes
 
 
-_unset = object()
-
-
-@contextmanager
-def temp_setting(
-    container: object, key: str, value: object
-) -> Generator[None, None, None]:
-    """Context manager to temporarily set a document setting."""
-    old_value = getattr(container, key, _unset)
-    setattr(container, key, value)
-    try:
-        yield
-    finally:
-        if old_value is _unset:
-            delattr(container, key)
-        else:
-            setattr(container, key, old_value)
-
-
 @contextmanager
 def temp_source_code(
     env: SphinxEnvType, source_code: str
 ) -> Generator[Path, None, None]:
     """Context manager to temporarily set the current document."""
-    fd, _p = mkstemp(suffix=".ipynb", text=True)
-    path = Path(_p)
-    with open(fd, "w", encoding="utf-8") as f:
-        f.write(source_code)
+    ext_data = cast(
+        "ExtData", env.current_document.setdefault("sphinx_exec_jupyter", {})
+    )
+    ext_data["count"] = ext_data.get("count", 0) + 1
+    old_docname = env.docname
+    docname_tmp = f"{old_docname}-exec-{ext_data['count']}"
 
-    docname_tmp = path.stem
+    d = Path(mkdtemp())
+    path = d / f"{docname_tmp}.ipynb"
+    path.parent.mkdir(parents=True, exist_ok=True)  # when docname contains slashes
+    path.write_text(source_code)
 
     def doc2path(docname: str, base: bool = True) -> Path:
         if docname == docname_tmp:
-            return Path(path)
+            return path
         return doc2path_orig(docname, base=base)
 
-    env.current_document.docname, old_docname = docname_tmp, env.docname
+    env.current_document.docname = docname_tmp
     env.doc2path, doc2path_orig = doc2path, env.doc2path
     try:
         yield path
     finally:
         env.current_document.docname = old_docname
         env.doc2path = doc2path_orig
-        path.unlink()
+        shutil.rmtree(d)
