5465e23a4e152f8997ff40af838f829a0413280d
Phil Schaf <flying-sheep@web.de>
2026-06-28 15:36:42 +0200

    handle all image types

--- src/sphinx_exec_jupyter/mime_handlers.py
+++ src/sphinx_exec_jupyter/mime_handlers.py
@@ -3,14 +3,17 @@
 
 from __future__ import annotations
 
-import struct
 from base64 import b64decode
 from itertools import chain
+from types import MappingProxyType
 from typing import TYPE_CHECKING, override
 
+from ipython.core import display
 from myst_nb.core.render import MimeRenderPlugin
 
 if TYPE_CHECKING:
+    from collections.abc import Callable, Mapping
+
     from docutils import nodes
     from myst_nb.core.render import MimeData, NbElementRenderer
 
@@ -18,14 +21,23 @@ if TYPE_CHECKING:
 HV_MIME_TYPES = frozenset(
     {"application/vnd.holoviews_load.v0+json", "application/vnd.holoviews_exec.v0+json"}
 )
-PNG_MIME_TYPE = "image/png"
+RASTER_IMAGE_DIMS: Mapping[str, Callable[[bytes], tuple[int, int]] | None] = (
+    MappingProxyType(
+        {
+            "image/png": getattr(display, "_pngxy", None),
+            "image/jpeg": getattr(display, "_jpegxy", None),
+            "image/gif": getattr(display, "_gifxy", None),
+            "image_webp": getattr(display, "_webpxy", None),
+        }
+    )
+)
 
 
 class SEJMimeRenderer(MimeRenderPlugin):
-    """Accepts but ignores HoloViews metadata and adds dimensions to PNGs."""
+    """Accepts but ignores HoloViews metadata and adds dimensions to images."""
 
     mime_priority_overrides = tuple(
-        ("*", mt, 1) for mt in chain(HV_MIME_TYPES, [PNG_MIME_TYPE])
+        ("*", mt, 1) for mt in chain(HV_MIME_TYPES, RASTER_IMAGE_DIMS)
     )
 
     @staticmethod
@@ -35,19 +47,13 @@ class SEJMimeRenderer(MimeRenderPlugin):
     ) -> None | list[nodes.Element]:
         if not inline and data.mime_type in HV_MIME_TYPES:
             return []
-        if not inline and data.mime_type == PNG_MIME_TYPE:
+        if not inline and (get_dims := RASTER_IMAGE_DIMS.get(data.mime_type)):
             [elem] = renderer.render_image(data)
             if not (elem.attributes.keys() & {"width", "height"}):
                 assert isinstance(data.content, str)
-                width, height = _pngxy(b64decode(data.content))
+                data_bytes = b64decode(data.content)
+                width, height = get_dims(data_bytes)
                 elem["width"] = str(width)
                 elem["height"] = str(height)
             return [elem]
         return None
-
-
-def _pngxy(data: bytes) -> tuple[int, int]:
-    """Read the (width, height) from a PNG header."""
-    ihdr = data.index(b"IHDR")
-    # next 8 bytes are width/height
-    return struct.unpack(">ii", data[ihdr + 4 : ihdr + 12])
