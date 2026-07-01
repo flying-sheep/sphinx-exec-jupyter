032f403834a839c557a603f0badfb0c1d6977867
Phil Schaf <flying-sheep@web.de>
2026-07-01 10:15:10 +0200

    more

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,8 @@ from __future__ import annotations
 
 import os
 from importlib.metadata import metadata
+from pathlib import PurePosixPath
+from urllib.parse import urlparse, urlunparse
 
 _meta = metadata("sphinx-exec-jupyter")
 project = _meta["name"]
@@ -23,10 +25,17 @@ master_doc = "index"
 
 html_theme = "furo"
 
+_gh_url = urlparse(os.environ.get("READTHEDOCS_GIT_CLONE_URL", "").removesuffix(".git"))
+
 html_theme_options = dict(
-    source_repository=os.environ.get("READTHEDOCS_GIT_CLONE_URL", "").removesuffix(
-        ".git"
-    ),
-    source_branch=os.environ.get("READTHEDOCS_GIT_IDENTIFIER", ""),
+    source_repository=urlunparse(_gh_url),
+    source_branch=os.environ.get("READTHEDOCS_GIT_IDENTIFIER"),
     source_directory="docs",
+)
+html_context = dict(
+    github_user=PurePosixPath(_gh_url.path).parts[0],
+    github_repo=PurePosixPath(_gh_url.path).parts[1],
+    github_version=os.environ.get("READTHEDOCS_GIT_IDENTIFIER"),
+    current_version=os.environ.get("READTHEDOCS_VERSION"),
+    slug=os.environ.get("READTHEDDOCS_PROJECT"),
 )
