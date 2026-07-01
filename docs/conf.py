32eec843743916cfd6aa17e648bffe936a777401
Phil Schaf <flying-sheep@web.de>
2026-07-01 09:59:56 +0200

    docs: add GH link

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@
 
 from __future__ import annotations
 
+import os
 from importlib.metadata import metadata
 
 _meta = metadata("sphinx-exec-jupyter")
@@ -21,3 +22,11 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 master_doc = "index"
 
 html_theme = "furo"
+
+html_theme_options = dict(
+    source_repository=os.environ.get("READTHEDOCS_GIT_CLONE_URL", "").removesuffix(
+        ".git"
+    ),
+    source_branch=os.environ.get("READTHEDOCS_GIT_IDENTIFIER", ""),
+    source_directory="docs",
+)
